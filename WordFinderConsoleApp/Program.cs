c20c957ca398bcda2ff4e546f1f88e3911620a90
Shams Mukhtar <shams.mukhtar@outlook.com>
2025-01-27 21:34:13 -0500

    Minor update in the output.

--- WordFinderConsoleApp/Program.cs
+++ WordFinderConsoleApp/Program.cs
@@ -13,16 +13,17 @@ IEnumerable<string> matrix = new[]
 
 IEnumerable<string> wordstream = new[]
 {
-    "chill", "cold", "wind", "win", "old"
+    "chill", "cold", "wind", "win", "old", "india"
 };
 
 IWordFinder wordFinder = new WordFinder(matrix);
 var result = wordFinder.Find(wordstream);
 
+var i = 0;
 Console.WriteLine("Top Found Words:");
 foreach (var word in result)
 {
-    Console.WriteLine(word);
+    Console.WriteLine($"{++i}\t{word}");
 }
 
 Console.ReadKey();
